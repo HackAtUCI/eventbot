5b790950006b10b491ae032787ad4a360ce51716
Chase Carnaroli <chasecarnaroli@gmail.com>
2021-06-12 21:53:09 -0700

    Add input fields for token and message
    Token input field appears when no token is in local storage.
    Message input field posts a message to #bot-playground.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+
+const { WebClient } = require('@slack/web-api');
 
 function App() {
+  const [token, setToken] = useState(localStorage.getItem('slackToken'));
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    // Save Slack token to local storage for future use
+    localStorage.setItem('slackToken', token);
+  }, [token])
+
+  // Create WebClient to interface with Slack API
+  const slackClient = new WebClient(token);
+
+  const saveToken = event => {
+    event.preventDefault();
+    setToken(event.target[0].value);
+  }
+
+  const sendMessage = event => {
+    event.preventDefault();
+
+    slackClient.chat.postMessage({
+      text: message,
+      channel: "C01AU7UCNGN",
+    })
+
+    setMessage('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!token &&
+        <div>
+          <h1>Enter Slack Token</h1>
+          <form onSubmit={saveToken}>
+            <input type="password" />
+            <input type="submit" />
+          </form>
+        </div>
+      }
+      {token &&
+        <div>
+          <h1>Send message to #bot-playground</h1>
+          <form onSubmit={sendMessage}>
+            <input type="text" value={message} onChange={event => {setMessage(event.target.value)}} />
+            <input type="submit" />
+          </form>
+        </div>
+      }
     </div>
   );
 }
