507535e4b0992cddd977d69ed430cf695dbf29ec
Chase Carnaroli <chasecarnaroli@gmail.com>
2021-06-18 03:38:57 -0700

    Wait until SlackClient has been loaded until attempting to access it

--- src/views/scheduleMessage.js
+++ src/views/scheduleMessage.js
@@ -5,13 +5,13 @@ import { useContext, useEffect, useRef, useState } from 'react';
 import AppContext from '../AppContext';
 
 function ScheduleMessage() {
-    const {slackClient, workspace} = useContext(AppContext);
+    const {slackClient, workspace, isLoading} = useContext(AppContext);
     const [scheduledMessages, setScheduledMessages] = useState([]);
     const timeInput = useRef(null)
     
     useEffect(()=>{
-        loadScheduledMessages();
-    }, [])
+        if (!isLoading) {loadScheduledMessages()};
+    }, [isLoading])
 
     const loadScheduledMessages = () => {
         slackClient.getScheduledMessages().then(messages => {
@@ -36,6 +36,7 @@ function ScheduleMessage() {
             <MessageInput submitAction={scheduleMessage} />
             <h3>Scheduled Messages</h3>
             <table>
+                <tbody>
                 <tr>
                     <th>ID</th>
                     <th>Channel</th>
@@ -56,6 +57,7 @@ function ScheduleMessage() {
                         </tr>
                     )
                 })}        
+                </tbody>       
             </table>
         </div>
     );
